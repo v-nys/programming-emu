51486f1ca1a98eb97d7886721603c7e11dc3d9ee
Vincent Nys <vincentnys@gmail.com>
2017-04-30 15:31:03 +0200

    This inserts a TOC element, but it requires that toc is a function. I want it to just be a variable.

--- pollen.rkt
+++ pollen.rkt
@@ -1,7 +1,9 @@
 #lang racket/base
-(require pollen/decode pollen/misc/tutorial txexpr)
-(provide root)
+(require pollen/decode pollen/misc/tutorial pollen/pagetree txexpr)
 (define (root . elements)
-   (txexpr 'root empty (decode-elements elements
-     #:txexpr-elements-proc decode-paragraphs
-     #:string-proc (compose1 smart-quotes smart-dashes))))
+  (txexpr 'root empty (decode-elements elements
+                                       #:txexpr-elements-proc decode-paragraphs
+                                       #:string-proc (compose1 smart-quotes smart-dashes))))
+(provide root)
+(define (toc) (txexpr 'toc empty (next* 'toc.html (get-pagetree "index.ptree"))))
+(provide toc)
--- toc.html.pm
+++ toc.html.pm
@@ -1,3 +1,3 @@
 #lang pollen
-◊(define-meta template "toc-template.html")
 ◊h1{Table of contents}
+◊toc{}
