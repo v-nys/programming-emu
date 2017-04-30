a2b25fd6f791760fe1e8c4379a3c8870d4875ade
Vincent Nys <vincentnys@gmail.com>
2017-04-30 15:59:33 +0200

    This gives a list to which I can add the title.

--- pollen.rkt
+++ pollen.rkt
@@ -1,12 +1,19 @@
 #lang racket/base
-(require pollen/decode pollen/misc/tutorial pollen/pagetree txexpr)
+(require pollen/core pollen/decode pollen/misc/tutorial pollen/pagetree txexpr)
 (define (root . elements)
   (txexpr 'root empty (decode-elements elements
                                        #:txexpr-elements-proc decode-paragraphs
                                        #:string-proc (compose1 smart-quotes smart-dashes))))
 (provide root)
 ; TODO: find out why get-pagetree only works when this thing is a function
-(define (toc) (txexpr 'toc empty (next* 'toc.html (get-pagetree "index.ptree"))))
+; TODO: don't just use the source file name, also include the title
+; select 'h1 p should work
+; so: map over next -> tocentry filename-as-string title-as-string
+;(define (toc) (txexpr 'toc empty (next* 'toc.html (get-pagetree "index.ptree"))))
+(define (toc)
+  (txexpr 'toc empty
+          (map (λ (fn) (txexpr 'tocentry empty (list fn)))
+               (next* 'toc.html (get-pagetree "index.ptree")))))
 (provide toc)
 
 (module setup racket/base
