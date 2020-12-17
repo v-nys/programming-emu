4ba2647c59ba15d06c817967f36c0ef202c3bc3b
Vincent Nys <vincentnys@gmail.com>
2020-12-17 09:09:02 +0100

    Via build script wordt initSQLiteDB rechtstreeks uitgevoerd, dus logisch in main module te zetten.

--- initSQLiteDB.rkt
+++ initSQLiteDB.rkt
@@ -1,9 +1,9 @@
 #lang racket
 (require db)
-(define sqlc
-  (sqlite3-connect #:database "db.sqlite" #:mode 'create))
-(query-exec sqlc
-  "CREATE TABLE IF NOT EXISTS Glossary(term varchar(100), explanation varchar(500))")
-(query-exec sqlc
-  "CREATE TABLE IF NOT EXISTS Titles(pagenode varchar(500), title varchar(500))")
-; (query-exec sqlc "INSERT INTO glossary(term,explanation) VALUES ('boheemse herder', 'kleine tsjechische voorouder van de duitse herder')")
+(module+ main
+  (define sqlc
+    (sqlite3-connect #:database "db.sqlite" #:mode 'create))
+  (query-exec sqlc
+              "CREATE TABLE IF NOT EXISTS Glossary(term varchar(100), explanation varchar(500))")
+  (query-exec sqlc
+              "CREATE TABLE IF NOT EXISTS Titles(pagenode varchar(500), title varchar(500))"))
