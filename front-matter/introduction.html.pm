35396137b72efd83d2ffde00dd2c1f626cc31029
Vincent Nys <vincentnys@gmail.com>
2020-12-22 11:03:29 +0100

    Introductie nagekeken.

--- front-matter/introduction.html.pm
+++ front-matter/introduction.html.pm
@@ -1,7 +1,7 @@
 #lang pollen
 ◊;{MIT License
 
-Copyright (c) 2017 Vincent Nys
+Copyright (c) 2017-2020 Vincent Nys
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -24,10 +24,12 @@ SOFTWARE.}
 ◊img[#:class "u-emu-svg" #:id "emu-svg" #:src "/static/images/emu.svg"]
 I'm a poor ◊a[#:href "http://www.racket-lang.org"]{Racket} programmer. If you give me an end-to-end task, I can probably write code that accomplishes it, but said code may be orders of magnitude longer, buggier and slower than that of a truly skilled Racket user. I can figure out what I need to do at compile time and what I need to do at runtime, but often through trial and error. I only have ◊a[#:href "https://github.com/v-nys/cclp"]{one moderately large Racket codebase} and I'm extremely embarrassed to point you to it. Those are the facts, so what to do?
 
-It's well established that acquiring a skill requires ◊a[#:href "http://expertenough.com/1423/deliberate-practice"]{deliberate practice}. If you want to learn how to do something general (like writing decent Racket code), it's important to have specific goals, to make a conscious effort to learn from mistakes and to seek out feedback. Enter ◊work{Programming Emu}, a project-based book in which I try to reverse engineer interesting Racket projects.
+It's well established that acquiring a skill requires ◊a[#:href "http://expertenough.com/1423/deliberate-practice"]{deliberate practice}. Even if you want to learn how to do something general (like writing decent Racket code), it's important to have specific goals, to make a conscious effort to learn from mistakes and to seek out feedback. Enter ◊work{Programming Emu}, a project-based book in which I try to reverse engineer interesting Racket projects.
 
-My hope is that, by carefully studying the code of experienced Racketeers, I'll be able to pick up new techniques and better programming habits. Writing a book about the process helps me stay focused on a particular project, it forces me to formulate my findings clearly and it leaves a record for myself and for you. Reverse engineering should serve as proof that I have sufficiently grasped the original code base. I encourage you to write your own implementation of the projects as we go along. To prove to yourself that you've mastered the material, rebuild the project from scratch and maybe even extend it.
+My hope is that, by carefully studying the code of experienced Racketeers, I'll be able to pick up new techniques and better programming habits. Writing a book about the process helps me stay focused on a particular project, it forces me to formulate my findings clearly and it leaves a record for myself and for you. Rebuilding the code base is my way of proving to myself that I have properly grasped the project. Therefore, I encourage you to write your own implementation of the projects as we go along. To prove to yourself that you've mastered the material, rebuild the project from scratch and maybe even extend it or take it in a new direction.
 
-Each chapter will deal with a single project and the chapters will be more or less sorted based on the difficulty of the examined codebase. We'll dive into logic programming with ◊work{Parenlog}.
+Each chapter will deal with a project and the chapters will be more or less sorted based on the difficulty of the examined codebase.
 
-The code for this book is on ◊a[#:href "https://github.com/v-nys/programming-emu"]{Github} and I welcome bug reports, feedback about my code, requests to take on specific projects or pull requests. And I would ◊em{love} questions about why I implemented something in one way and not in another. Also, don't be reluctant to contribute if you're not particularly experienced. After all, my own lack of experience is the whole reason I'm currently writing this.
+We'll dive into logic programming with ◊work{Parenlog}.
+
+There is a glossary to help you out with terminology and the code for this book is on ◊a[#:href "https://github.com/v-nys/programming-emu"]{Github}. I welcome bug reports, feedback about my code, requests to take on specific projects or pull requests. And I would ◊em{love} questions about why I implemented something in one way and not in another. Also, don't be reluctant to contribute if you're not particularly experienced. After all, my own lack of experience with Racket is the whole reason I started writing this book.
--- index.html.pm
+++ index.html.pm
@@ -2,21 +2,3 @@
 ◊(require pollen/unstable/convert)
 ◊title[metas]{Table of contents}
 ◊(toc-for-descendants metas)
-
-◊div[#:id "mc_embed_signup"]{
- ◊form[#:action "//telenet.us16.list-manage.com/subscribe/post?u=a12fcdf12f48c319029ffe244&amp;id=d25ea0e7ac" #:class "validate" #:id "mc-embedded-subscribe-form" #:method "post" #:name "mc-embedded-subscribe-form" #:novalidate "novalidate" #:target "_blank"]{
-  ◊div[#:id "mc_embed_signup_scroll"]{
-   ◊span[#:id "subscribe-header-span"]{Subscribe for updates about completed chapters}
-   ◊div[#:class "mc-field-group"]{
-    ◊label[#:for "mce-EMAIL"]{Email Address }
-    ◊input[#:class "required email" #:id "mce-EMAIL" #:name "EMAIL" #:type "email" #:value ""]{
-   }}
-   ◊div[#:class "clear" #:id "mce-responses"]{
-    ◊div[#:class "response" #:id "mce-error-response" #:style "display:none"]
-    ◊div[#:class "response" #:id "mce-success-response" #:style "display:none"]
-   }    
-   ◊div[#:aria-hidden "true" #:style "position: absolute; left: -5000px;"]{◊input[#:name "b_a12fcdf12f48c319029ffe244_d25ea0e7ac" #:tabindex "-1" #:type "text" #:value ""]}
-   ◊div[#:class "clear"]{◊input[#:class "button" #:id "mc-embedded-subscribe" #:name "subscribe" #:type "submit" #:value "Subscribe"]}
-  }
- }
-}
