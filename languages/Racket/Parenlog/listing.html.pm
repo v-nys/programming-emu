7aa96a35543454def377160a5f158a2acb75d65a
Vincent Nys <vincentnys@gmail.com>
2020-12-22 22:02:20 +0100

    Don't need to provide full code. It's online, anyway.

--- languages/Racket/Parenlog/listing.html.pm
+++
@@ -1,8 +0,0 @@
-#lang pollen
-◊title[metas]{Complete listings}
-What's that? What I wrote in the preceding sections is merely a collection of incoherent ramblings? In that case, here's a full source code listing for the final runtime-based implementation of Parenlog.
-
-Here's a full listing for the syntax-based implementation.
-
-And if you're interested in the revision history of both of these, feel free to poke through the ◊a{Github repository}. The main branch contains the runtime approach and there is a separate branch for the syntax-based approach.
-◊todo{Push repository parenlogsecondtime to Github.}
